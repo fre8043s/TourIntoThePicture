fcd121f0cc2388bfa99aab19472b732109f3e754
glfgeorge <george0513@126.com>
2022-06-22 18:16:52 +0200

    draw vanish points and radial lines and inner rect

--- test.m
+++ test.m
@@ -1,4 +1,4 @@
-image = imread('..\\..\\CV-Challenge-22-Datensatz\oil-painting.png');
+image = imread('..\\\CV-Challenge-22-Datensatz\oil-painting.png');
 imshow(image);
 hold on
 % specify the outer rectangle
@@ -6,24 +6,26 @@ oup_left = [0, 0];
 odown_left = [0, size(image,1)];
 oup_right = [size(image,2), 0];
 odown_right = [size(image,2), size(image,1)];
-plot([oup_left(1),odown_left(1),odown_right(1),oup_right(1),oup_left(1)],[oup_left(2),odown_left(2),odown_right(2),oup_right(2),oup_left(2)],'LineWidth',2,'color','blue')
+corners = [oup_left; oup_right; odown_right; odown_left];
+rectangle('Position', [0,0,size(image,2),size(image,1)],'LineWidth',2,'EdgeColor','blue');
 
 % specify the vanish point and draw the radial lines
-vanish_point = specify_vanish_point();
+vanish_point = specify_vanish_point(image, corners);
 % specify the outer rectangle
-p = specify_inner_rectangle();
+p = specify_inner_rectangle(image);
 
 %% specify the vanishing point and inner Rectangle
-function vanish_point = specify_vanish_point()
+function vanish_point = specify_vanish_point(image, corners)
     [x,y] = getpts;
     vanish_point = [round(x), round(y)];
     plot(vanish_point(1),vanish_point(2),'*','color','blue');
     % draw the radial lines
-    theta = 0:0.1:2*pi;
-    rho = 1;
-    polarplot(theta, rho)
+    for i = 1:length(corners)
+        radial_line = [vanish_point;corners(i,:)];
+        plot(radial_line(:,1),radial_line(:,2),'LineWidth',2,'color','blue');
+    end
 end
-function p = specify_inner_rectangle()
+function p = specify_inner_rectangle(image)
     p = getrect;
     p = round(p);
     rectangle('Position', p, 'EdgeColor', 'blue');
